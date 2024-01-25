f4a7721f09791393bbe58ed0b556087c4c007916
Ryan Bell <scruffyandbarney@gmail.com>
2024-01-25 20:55:25 +0000

    Extra comments

--- game.py
+++ game.py
@@ -1,16 +1,22 @@
 import pygame
 
+#I dont know what this does - maybe look at it later
 pygame.init()
+
+#Creates a window of size 1280,720
 screen = pygame.display.set_mode((1280, 720))
+#Keeps track of game time
 clock = pygame.time.Clock()
 running = True
 
 while running:
 
+    #Every frame we check the list of events, there are events such as button or moust pressed.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
+    #Fill the screen
     screen.fill('pink')
 
     #First step is to draw the board
@@ -22,7 +28,9 @@ while running:
     #How do we repeat this code to draw multiple cells 
     #How do we get this into a board shape
 
+    #Confusing name, but this draws all the stuff to the display
     pygame.display.flip()
+    #Keeps fps at 60
     clock.tick(60)
 
 pygame.quit()
